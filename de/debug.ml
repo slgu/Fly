82864e1d38d2442fc892988bc41a6520271dc975
Xin Xu <xx2168@columbia.edu>
2016-03-25 17:24:57 -0400

    replace list.itr with list.fold_left

--- de/debug.ml
+++ de/debug.ml
@@ -19,28 +19,30 @@ let string_of_op = function
     | RArrow -> "->"
     | LArrow -> "<-"
     | SAdd -> "add string"
-    | _ -> "nothing"
 
 let string_of_uop = function
     | Neg -> "neg"
     | Not -> "not"
 
 let rec debug_expr = function
-    | Literal a -> print_endline ("a integer:" ^ (string_of_int a))
-    | BoolLit a -> if a = true then print_endline "a bool:true" else print_endline "a bool:false"
-    | Id a -> print_endline ("an id:" ^ a)
-    | Set a -> print_endline "a set:";List.iter debug_expr a
-    | Map a -> print_endline "a set:";List.iter (fun (x,y) -> print_endline "kv";debug_expr x;debug_expr y) a
-    | Array a -> print_endline "an array:";List.iter debug_expr a
-    | Binop (a, op, b) -> print_endline ("binop:" ^ (string_of_op op));
-        print_endline "left:";debug_expr a;debug_expr b
-    | Unop (uop, a) -> print_endline ("unop:" ^ (string_of_uop uop));
-        debug_expr a
-    | Assign (a, b) -> print_endline ("assign: " ^ a ^" by:");debug_expr b
-    | Call (id, exprs) -> print_endline ("call: " ^ id);List.iter debug_expr exprs;
-    | ObjCall (id1, id2, exprs) -> print_endline ("call by" ^ id1 ^ "." ^ id2);List.iter debug_expr exprs;
-    | Func (a, b) -> print_endline "lambda:";List.iter print_endline a;print_endline "lambda expr:";debug_expr b
+    | Literal a -> "a integer: " ^ (string_of_int a)
+    | BoolLit a -> if a = true then "a bool: true" else "a bool: false"
+    | Float a -> "a float: " ^ (string_of_float a)
+    | Id a -> "an id: " ^ a
+    | Set a -> "a set: " ^ List.fold_left (func str item -> str ^ ", " ^ item) "" (List.map debug_expr a)
+    | Map a -> "a map: " ^ List.fold_left (func str item -> str ^ ", " ^ item) "" (List.map (fun (x,y) -> "(" ^ debug_expr x ^ "," ^ debug_expr y ^ ")") a)
+    | Array a -> "an array: " ^ List.fold_left (func str item -> str ^ ", " ^ item) "" (List.map debug_expr a)
+    | String a -> "a string: " ^ a
+    | Binop (a, op, b) -> "binop:" ^ (string_of_op op) ^ ", left:"^ debug_expr a^ ", right:" ^ debug_expr b
+    | Unop (uop, a) -> "unop: " ^ (string_of_uop uop) ^ debug_expr a
+    | Call (id, exprs) -> "call: " ^ id ^ "; expressions: " List.fold_left (func str item -> str ^ ", " ^ item) "" (List.map debug_expr exprs);
+    | ObjCall (id1, id2, exprs) -> "call by" ^ id1 ^ "." ^ id2 ^ "; expressions: " ^ List.fold_left (func str item -> str ^ ", " ^ item) "" (List.map debug_expr exprs)
+    | Func (a, b) -> "lambda: " ^ List.fold_left (func str item -> str ^ ", " ^ item) "" (List.map debug_expr a) ^ "; lambda expr: " ^ debug_expr b
+    | Assign (a, b) -> "assign: " ^ a ^" by: " ^ debug_expr b
+    | ListComprehen (a, b, c) -> "list comprehension: " ^ debug_expr a ^ b ^ debug_expr c;
     | _ -> print_endline "nothing"
+
+
 
 let debug_stmt = function
     | Expr a -> debug_expr a
@@ -52,10 +54,8 @@ let debug_fdecl (fdecl : func_decl) = match fdecl with
 let debug_ast = function
     | Program (cdecls, fdecls) -> List.iter debug_fdecl fdecls
 
-(* debug debug.ml
 let _ =
     let a = BoolLit true and b = Set ([BoolLit true;BoolLit false;BoolLit true])
     and c  = Map ([Id "a", BoolLit true;Id "b", BoolLit false;
         Id "c", BoolLit true])
     in debug_expr c
-    *)
