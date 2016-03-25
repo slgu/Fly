28242f64d544b85b9078e71de102a1784ad51073
Hsiang-Ho Lin <mymaydayya@gmail.com>
2016-03-25 16:15:48 -0400

    Add support for return and cout

--- check/infer2.ml
+++ check/infer2.ml
@@ -3,8 +3,8 @@ open Ast
 open Sast
 
 let infer_check (ast : program) = (* return function list *)
-    let body = [TExpr(TCall(("print",[TString("Hello World")]), Void));TReturn(TLiteral(0))] in
+    let body = [TExpr(TCall(("coolfunc",[TString("Hello World")]), Void));TReturn(TLiteral(0))] in
     let mainfunc = {tret=Int;tbody=body;tformals=[];tfname="main";ttkey=""} in
-    let anobody = [TExpr(TAssign(("val", TLiteral(0)),Int));TReturn(TId("val",Int))] in
-    let anotherfunc = {tret=Int;tbody=anobody;tformals=[("a",Int);("b",String)];tfname="cool";ttkey=""} in
+    let anobody = [TExpr(TAssign(("ret", TLiteral(0)),Int));TExpr(TCall(("print",[TId("b",String)]), Void));TReturn(TId("ret",Int))] in
+    let anotherfunc = {tret=Int;tbody=anobody;tformals=[("b",String)];tfname="coolfunc";ttkey=""} in
     [anotherfunc;mainfunc]
