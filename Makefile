557673980199efd221556d6389eb8d159c4181d7
Hsiang-Ho Lin <mymaydayya@gmail.com>
2016-03-26 22:06:46 -0400

    Add test code for generating hashtable containing Sast.t_func_decl

--- Makefile
+++ Makefile
@@ -24,11 +24,10 @@ gen:
 	ocamlc -I $(ast_dir) -c $(parser_dir)/parser.mli
 	ocamlc -I $(parser_dir) -c $(parser_dir)/scanner.ml
 	ocamlc -I $(ast_dir) -I $(parser_dir) -c $(parser_dir)/parser.ml
-	ocamlc -I $(ast_dir) -c $(debug_dir)/debug.ml
 	ocamlc -I $(ast_dir) -c $(check_dir)/infer2.ml
 	ocamlc -I $(ast_dir) -c $(gen_dir)/codegen.ml
 	ocamlc -I $(parser_dir) -I $(check_dir) -I $(gen_dir) -c fly_testgen.ml
-	ocamlc -o fly $(parser_dir)/scanner.cmo $(parser_dir)/parser.cmo $(debug_dir)/debug.cmo $(ast_dir)/ast.cmo $(check_dir)/infer2.cmo $(gen_dir)/codegen.cmo fly_testgen.cmo
+	ocamlc -o fly $(parser_dir)/scanner.cmo $(parser_dir)/parser.cmo $(ast_dir)/ast.cmo $(check_dir)/infer2.cmo $(gen_dir)/codegen.cmo fly_testgen.cmo
 	rm $(parser_dir)/scanner.ml $(parser_dir)/parser.mli $(parser_dir)/parser.ml $(parser_dir)/parser.output */*.cm* *.cm*
 	cat test/test1 | ./fly
 debug:
--- check/infer2.ml
+++ check/infer2.ml
@@ -2,6 +2,27 @@
 open Ast
 open Sast
 
+let (func_binds : (string, t_func_decl) Hashtbl.t) = Hashtbl.create 16
+
+(*
+type t_func_decl = {
+        ttkey: string; (* for matching*)
+        tfname: string;
+        tformals: (string * typ) list;
+        tbody: tstmt list;
+        tret: typ (*the return value type*)
+    }
+*)
+
+let gen_example_ht =
+    let body = [TExpr(TCall(("coolfunc",[TString("Hello World")]), Void));TReturn(TLiteral(0))] in
+    let mainfunc = {tret=Int;tbody=body;tformals=[];tfname="main";ttkey=""} in
+    let anobody = [TExpr(TAssign(("ret", TLiteral(0)),Int));TExpr(TCall(("print",[TId("b",String)]), Void));TReturn(TId("ret",Int))] in
+    let anotherfunc = {tret=Int;tbody=anobody;tformals=[("b",String)];tfname="coolfunc";ttkey=""} in
+    let flist = [mainfunc;anotherfunc] in
+    List.iter 
+        (fun fdecl -> match fdecl with | {tfname = name ; _} -> Hashtbl.add func_binds name fdecl) flist
+
 let infer_check (ast : program) = (* return function list *)
     let body = [TExpr(TCall(("coolfunc",[TString("Hello World")]), Void));TReturn(TLiteral(0))] in
     let mainfunc = {tret=Int;tbody=body;tformals=[];tfname="main";ttkey=""} in
