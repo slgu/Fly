f5413e17f059248320678e3c9232fd01616fc676
karollyn <775461329@qq.com>
2016-04-08 13:35:09 -0400

    O(∩_∩)O~

--- compare_result.py
+++ compare_result.py
@@ -1,9 +1,10 @@
 import os
+import sys
 
 test_dir = 'test'
-cpp_results_dir = 'cpp_test_results'
+test_answer_dir = 'test_answer'
 fly_results = 'fly_results.txt'
-##cpp_results = 'cpp_results.txt'
+comma = ', '
 
 command1 = 'make'
 command2 = './Fly < test/'
@@ -11,7 +12,12 @@ command3 = ' > test.cpp'
 command4 = 'g++ test.cpp'
 command5 = './a.out > ' + fly_results #put the results from running fly code into 'fly_result.txt'
 
-for filename in os.listdir(test_dir):
+test_list = os.listdir(test_dir)
+if len(sys.argv) > 1:
+	test_list = sys.argv[1:]
+#print comma.join(test_list)
+
+for filename in test_list:
 	print filename
 	os.system(command1)
 	os.system(command2 + filename + command3)
@@ -20,15 +26,21 @@ for filename in os.listdir(test_dir):
 
 	file_fly_results = open(fly_results, 'r')
 	list_fly_results = file_fly_results.read().split()
-	print list_fly_results
 
-	list_cpp_results = open()......???????
+	filename_answer_results = test_answer_dir + '/' + filename
+	file_answer_results = open(filename_answer_results, 'r')
+	list_answer_results = file_answer_results.read().split()
 
-	if(list_fly_results == list_cpp_results):
-		print filename, 'correct result'
+	if(list_fly_results == list_answer_results):
+		print filename, ': correct result'
 	else:
-		print filename, 'incorrect result'
-	print ''
+		print filename, ': incorrect result'
+		print '\nfly_results: \n', comma.join(list_fly_results)
+		print '\nanswer: \n', comma.join(list_answer_results)
+	print '\n'
+
+os.remove('test.cpp')
+os.remove(fly_results)
 
 
 
