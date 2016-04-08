0e9c205351fae1ad38f6c9c4dbf868f8156b5942
karollyn <775461329@qq.com>
2016-04-08 16:39:34 -0400

    w(ﾟДﾟ)w

--- codegen/buildin.ml
+++ codegen/buildin.ml
@@ -2,13 +2,30 @@ open Ast
 (string, ((typ list), typ) list) list
 
 let build_in_code =
-"float to_float(int a) {
+float to_float(int a) {
     return float(a);
 }
 
 float to_float(string a) {
 
-}"
+}
+string to_string(int a ){
+	return to_string(a);
+}
+string to_string(float a ){
+	return to_string(a);		
+}
+string to_string(bool a){
+	return to_string(a);	
+}
+void exit(int exit_code){
+	exit(exit_code);
+}
+
+(*string to_string(void a){} *)
+(*string to_string(array a){} *)
+(*string to_string(set a ){} *)
+
 
 let build_in_func =
     [t_func_decl]
